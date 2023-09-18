77c53a91ac38895ab80b6941d93c1d8d071b40ce
taffarelcatao <taffarel-182@hotmail.com>
2023-09-18 12:24:41 -0400

    finalizando calendar

--- lib/src/futures/employee/schedule/widgets/schedule_calendar.dart
+++ lib/src/futures/employee/schedule/widgets/schedule_calendar.dart
@@ -1,9 +1,18 @@
-import 'package:dw_barbershop/src/core/ui/constants.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dw_barbershop/src/core/ui/helpers/messages.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+import 'package:dw_barbershop/src/core/ui/constants.dart';
+
 class ScheduleCalendar extends StatefulWidget {
-  const ScheduleCalendar({super.key});
+  final VoidCallback cancelPressed;
+  final ValueChanged<DateTime> okPressed;
+  const ScheduleCalendar({
+    Key? key,
+    required this.cancelPressed,
+    required this.okPressed,
+  }) : super(key: key);
 
   @override
   State<ScheduleCalendar> createState() => _ScheduleCalendarState();
@@ -11,9 +20,15 @@ class ScheduleCalendar extends StatefulWidget {
 
 class _ScheduleCalendarState extends State<ScheduleCalendar> {
   DateTime? selectedDay;
+
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: const Color(0xffe6e2e9),
+        borderRadius: BorderRadius.circular(16),
+      ),
       child: Column(
         children: [
           TableCalendar(
@@ -45,6 +60,38 @@ class _ScheduleCalendarState extends State<ScheduleCalendar> {
                 shape: BoxShape.circle,
               ),
             ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              TextButton(
+                onPressed: widget.cancelPressed,
+                child: const Text(
+                  'Cancelar',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: ColorsConstants.brow,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  if (selectedDay == null) {
+                    Messages.showError('Por favor selecione um dia', context);
+                  }
+                  widget.okPressed(selectedDay!);
+                },
+                child: const Text(
+                  'Ok',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: ColorsConstants.brow,
+                  ),
+                ),
+              ),
+            ],
           )
         ],
       ),
